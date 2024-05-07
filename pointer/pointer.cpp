47c23c5275b5aee42e76e6520330cf60b6987e9b
jaddu17 <shafajaddu@gmail.com>
2024-05-07 11:32:58 +0700

    membuat int  main

--- pointer/pointer.cpp
+++ pointer/pointer.cpp
@@ -8,3 +8,18 @@ public:
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa mhs{ 1 };     // Object mhs
+    mhs.showNim();          // Member Access Operator
+
+    mahasiswa& refMhs = mhs;    // Pointer Reference refMhs
+    refMhs.nim = 2;             // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa* pMhs = &mhs;     // Pointer Dereference pMhs
+    pMhs->nim = 3;              // Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
